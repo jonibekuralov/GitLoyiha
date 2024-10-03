b12f2265b114a2bab457a1f33e3df9dfe55ffe23
jonibekuralov <anonymousbycreated@gmail.com>
2024-10-03 10:08:19 +0500

    main ga qoyildi ozgarishlar

--- app/models.py
+++ app/models.py
@@ -73,3 +73,12 @@ class ContactData(models.Model):
 
     def __str__(self):
         return self.adress
+
+
+class FooterData(models.Model):
+    adress = models.CharField(max_length=150)
+    phone = models.CharField(max_length=50)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.adress
